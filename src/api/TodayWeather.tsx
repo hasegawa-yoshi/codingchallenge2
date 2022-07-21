2c8a424d3d64baf328a1bd5011a08e9780dbea33
hasegawa <101229468+hasegawa-yoshi@users.noreply.github.com>
2022-07-21 17:10:10 +0900

    今日の天気表示機能追加
    今日の天気にアイコン、最高気温、最低気温の表示を追加

--- src/api/TodayWeather.tsx
+++ src/api/TodayWeather.tsx
@@ -4,17 +4,25 @@ import { OpenWeatherInstance } from "./axios";
 
 type InWeather = {
   description: string;
+  icon: string;
+};
+
+type Inmain = {
+  temp_max: number;
+  temp_min: number;
 };
 
 type Weather = {
   name: string;
+  main: Inmain;
   weather: [InWeather];
 };
 
 const TodayWeather = () => {
   const [TodayWeather, setTodayWeather] = useState<Weather>({
     name: "",
-    weather: [{ description: "" }],
+    main: { temp_max: 0, temp_min: 0 },
+    weather: [{ description: "", icon: "" }],
   });
 
   useEffect(() => {
@@ -34,7 +42,13 @@ const TodayWeather = () => {
     <div style={{ margin: "auto" }}>
       <h2>今日の天気</h2>
       <p>{TodayWeather.name}</p>
+      <img
+        src={`http://openweathermap.org/img/w/${TodayWeather.weather[0].icon}.png`}
+        alt="weather"
+      />
       <p>{TodayWeather.weather[0].description}</p>
+      <p>最高気温:{TodayWeather.main.temp_max}</p>
+      <p>最低気温:{TodayWeather.main.temp_min}</p>
     </div>
   );
 };
