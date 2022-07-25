63902339aaf35c66b353037b56eff26d91bca7e2
hasegawa <101229468+hasegawa-yoshi@users.noreply.github.com>
2022-07-25 16:50:42 +0900

    週間予報実装
    OpenWeatherApiで今後８日間の天気を表示。アイコン、説明、気温まで。

--- src/api/WeekWeather.tsx
+++ src/api/WeekWeather.tsx
@@ -1,9 +1,83 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { OpenWeather_API_KEY } from "./ApiKeys";
+import { OneCallOpenWeatherInstance } from "./axios";
+
+type Inweather = {
+  description: string;
+  icon: string;
+};
+
+type Indaily = {
+  temp: { min: number; max: number };
+  weather: [Inweather];
+};
+
+type WeekWeatherType = {
+  daily: [
+    Indaily,
+    Indaily,
+    Indaily,
+    Indaily,
+    Indaily,
+    Indaily,
+    Indaily,
+    Indaily
+  ];
+};
 
 const WeekWeather = () => {
+  const [WeekWeather, setWeekWeather] = useState<WeekWeatherType>({
+    daily: [
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+    ],
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await OneCallOpenWeatherInstance.get(
+        `/onecall?lat=35&lon=137&exclude=current,minutely,hourly,alerts&appid=${OpenWeather_API_KEY}&lang=ja&units=metric`
+      );
+      setWeekWeather(request.data);
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(WeekWeather);
+
   return (
-    <div style={{ width: "100px", height: "100px", margin: "auto" }}>
-      週間予報
+    <div style={{ margin: "auto" }}>
+      <h2>週間予報</h2>
+      {WeekWeather.daily.map((day, i) => (
+        <Grid container direction="row" padding={2}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={2}>
+            {" "}
+            <img
+              src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`}
+              alt="weather"
+            />
+          </Grid>
+          <Grid item xs={3}>
+            {"  "}
+            <span>{day.weather[0].description}</span>
+          </Grid>
+          <Grid item xs={3}>
+            {" "}
+            <span>
+              {Math.round(day.temp.max)}/{Math.round(day.temp.min)}℃
+            </span>
+          </Grid>
+        </Grid>
+      ))}
     </div>
   );
 };
--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -3,3 +3,7 @@ import axios from "axios";
 export const OpenWeatherInstance = axios.create({
   baseURL: "http://api.openweathermap.org/data/2.5",
 });
+
+export const OneCallOpenWeatherInstance = axios.create({
+  baseURL: "http://api.openweathermap.org/data/2.5",
+});
