a2f6c1792f3e532800a13d5ce729bef7cdcfc7db
hasegawa <101229468+hasegawa-yoshi@users.noreply.github.com>
2022-07-26 14:40:24 +0900

    週間予報機能改善
    日付表示追加、900px以下でレイアウト変わるように調整

--- src/api/WeekWeather.tsx
+++ src/api/WeekWeather.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, Hidden } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { OpenWeather_API_KEY } from "./ApiKeys";
 import { OneCallOpenWeatherInstance } from "./axios";
@@ -10,6 +10,7 @@ type Inweather = {
 
 type Indaily = {
   temp: { min: number; max: number };
+  dt: number;
   weather: [Inweather];
 };
 
@@ -29,14 +30,46 @@ type WeekWeatherType = {
 const WeekWeather = () => {
   const [WeekWeather, setWeekWeather] = useState<WeekWeatherType>({
     daily: [
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
-      { temp: { min: 0, max: 0 }, weather: [{ description: "", icon: "" }] },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
+      {
+        temp: { min: 0, max: 0 },
+        dt: 0,
+        weather: [{ description: "", icon: "" }],
+      },
     ],
   });
 
@@ -53,25 +86,44 @@ const WeekWeather = () => {
 
   console.log(WeekWeather);
 
+  let jpdatearr: any = [];
+
+  WeekWeather.daily.map((day, i) => {
+    let date = new Date(day.dt * 1000);
+    let jpdate = date.toLocaleDateString();
+    jpdatearr[i] = jpdate;
+    return jpdatearr;
+  });
+
+  console.log(jpdatearr);
+
   return (
-    <div style={{ margin: "auto" }}>
+    <div style={{ textAlign: "center" }}>
       <h2>週間予報</h2>
       {WeekWeather.daily.map((day, i) => (
-        <Grid container direction="row" padding={2}>
-          <Grid item xs={2}></Grid>
+        <Grid
+          container
+          spacing={0.5}
+          alignItems="center"
+          justifyContent="center"
+          textAlign="center"
+          marginBottom={2}
+        >
+          <Grid item xs={12} md={2}>
+            {jpdatearr[i]}
+          </Grid>
           <Grid item xs={2}>
-            {" "}
             <img
               src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`}
               alt="weather"
             />
           </Grid>
-          <Grid item xs={3}>
-            {"  "}
-            <span>{day.weather[0].description}</span>
-          </Grid>
-          <Grid item xs={3}>
-            {" "}
+          <Hidden mdDown>
+            <Grid item md={3}>
+              <span>{day.weather[0].description}</span>
+            </Grid>
+          </Hidden>
+          <Grid item xs={12} md={3}>
             <span>
               {Math.round(day.temp.max)}/{Math.round(day.temp.min)}℃
             </span>
