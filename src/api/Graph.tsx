7850a7e47929d1dd9bab97e48838f8362a6e1025
hasegawa <101229468+hasegawa-yoshi@users.noreply.github.com>
2022-07-27 18:03:14 +0900

    グラフ機能実装
    グラフを描画、値は適当、次の更新でopenweatherAPIと紐付けする

--- src/api/Graph.tsx
+++ src/api/Graph.tsx
@@ -1,9 +1,62 @@
 import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 
 const Graph = () => {
+  Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const labels = [
+    "1時間後",
+    "2時間後",
+    "3時間後",
+    "4時間後",
+    "5時間後",
+    "6時間後",
+    "7時間後",
+    "8時間後",
+  ];
+  const graphData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "気温変化（℃）",
+        data: [65, 59, 60, 81, 56, 55, 100, 150],
+        borderColor: "rgb(75, 192, 192)",
+      },
+    ],
+  };
+
+  const options: {} = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "1時間毎の予想気温",
+      },
+    },
+  };
   return (
-    <div style={{ width: "100px", height: "100px", margin: "auto" }}>
-      予想気温グラフ
+    <div style={{ margin: "auto", width: "100%" }}>
+      <Line data={graphData} options={options} />
     </div>
   );
 };
